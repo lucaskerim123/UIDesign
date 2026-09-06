34e19c14fb44f97d462f2f4efade804ecf853581
lucaskerim123 <lucas.kerim@gmail.com>
2026-09-06 12:21:08 +1000

    Restrict Engine Host configuration to administrators

--- src/routes/engines/[engine]/configuration/+page.server.ts
+++ src/routes/engines/[engine]/configuration/+page.server.ts
@@ -1,8 +1,8 @@
-import { requireUser } from '$lib/server/auth';
+import { requireAdmin } from '$lib/server/auth';
 import { getEngineHubEngine } from '$lib/server/engine-hub';
 
 export async function load({ cookies, params }) {
-	const user = await requireUser(cookies);
+	const user = await requireAdmin(cookies);
 	const engine = await getEngineHubEngine(params.engine);
 	return { user, engine };
 }
